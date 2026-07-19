c2a2cf01a443f43ca125b30e55af38c1333b811e
AHMED TARIF <mr.arafat.x404@gmail.com>
2026-07-19 20:05:19 +0600

    Update ss.js

--- scripts/cmds/ss.js
+++ scripts/cmds/ss.js
@@ -6,6 +6,7 @@ module.exports = {
     author: "Jubayer",
     countDown: 3,
     role: 0,
+    noPrefix: true,
     shortDescription: "Take a website screenshot",
     longDescription: "Capture and send a live screenshot of any website",
     category: "Everyone",
@@ -15,7 +16,7 @@ module.exports = {
   onStart: async function ({ message, args }) {
     if (!args[0]) return message.reply("✅ Please provide a website URL\n\nExample: ss github.com");
 
-    const url = args[0].startsWith("http") ? args[0] : `https://${args[0]}`;
+    const url = args[0].startsWith("http") ? args[0] : ``;
     const imgUrl = `https://jubayer-screenshort-apis-xy.onrender.com/ss?url=${encodeURIComponent(url)}`;
 
     await message.reply({
